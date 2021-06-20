9daf71d6307ce39f2bf75fed0e70358f826a5413
jeonjonghyeok <tongher1685@gmail.com>
2021-06-21 00:21:19 +0900

    feat: signin api
    로그인 기능 구현 완료
    
    Auth 구현

--- token/upbit.go
+++ token/upbit.go
@@ -1,6 +1,9 @@
 package upbit
 
 import (
+	"errors"
+	"time"
+
 	"github.com/dgrijalva/jwt-go"
 	"github.com/twinj/uuid"
 )
@@ -12,7 +15,9 @@ type customClaims struct {
 	UserID    int    `json:"id"`
 }
 
-func New(id int, AccessKey, SecretKey string) (string, error) {
+const mySigningKey = "coinss"
+
+func NewUpbit(id int, AccessKey, SecretKey string) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, customClaims{
 		UserID:         id,
 		AccessKey:      AccessKey,
@@ -20,5 +25,36 @@ func New(id int, AccessKey, SecretKey string) (string, error) {
 		StandardClaims: jwt.StandardClaims{},
 	})
 	return token.SignedString([]byte(SecretKey))
+}
 
+func New(userID int) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, customClaims{
+		UserID: userID,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(30 * 24 * time.Hour).Unix(),
+			Issuer:    "jjh",
+		},
+	})
+	return token.SignedString([]byte(mySigningKey))
+}
+
+func Parse(token string) (userID int, err error) {
+	parsed, err := jwt.ParseWithClaims(token, &customClaims{},
+		func(token *jwt.Token) (interface{}, error) {
+			return []byte(mySigningKey), nil
+		})
+
+	if err != nil {
+		return 0, err
+	}
+
+	if !parsed.Valid {
+		return 0, errors.New("token is invalid")
+	}
+
+	if c, ok := parsed.Claims.(*customClaims); ok {
+		return c.UserID, nil
+	}
+
+	return 0, errors.New("token is invalid")
 }
